d9cab81415c5b5f6bbec9ec61e0f49e414a8323a
Steven Wade <stevenwadejr@gmail.com>
2014-09-25 09:44:20 -0400

    init commit

---
+++ app/routes.php
@@ -0,0 +1,19 @@
+<?php
+
+Route::group(['prefix' => 'lists'], function()
+{
+    Route::get('/{id}', 'ItemsController@show');
+    Route::post('/', 'ItemsController@create');
+    Route::put('/{id}', 'ItemsController@update');
+    Route::delete('/{id}', 'ItemsController@delete');
+})
+
+Route::group(['prefix' => 'lists'], function()
+{
+    Route::get('/', 'ListsController@listAll');
+    Route::get('/{id}', 'ListsController@showList');
+    Route::post('/', 'ListsController@createList');
+    Route::post('/{id}', 'ListsController@update');
+    Route::put('/{id}', 'ListsController@overwrite');
+    Route::delete('/{id}', 'ListsController@delete');
+});
